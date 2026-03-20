03af2ceaf95ea700d35486b04f7298d458c9616e
Atharva Nimbalkar <nimbalkaratharva@gmail.com>
2026-03-19 22:54:25 -0400

    modified controller to accept requests form different origins

--- UrlShortener/src/main/java/com/example/UrlShortener/Controller/UrlController.java
+++ UrlShortener/src/main/java/com/example/UrlShortener/Controller/UrlController.java
@@ -2,6 +2,7 @@ package com.example.UrlShortener.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.UrlShortener.Service.UrlService;
 import com.example.UrlShortener.Utility.Utility;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/url")
 public class UrlController {
